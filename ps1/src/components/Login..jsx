255461fb3a6979bbcd1d0f11e93349917c33d28a
mayankmishra221912 <mayankmishra4625@gmail.com>
2025-07-23 09:39:13 +0530

    my frontend

--- ps1/src/components/Login..jsx
+++ ps1/src/components/Login..jsx
@@ -2,11 +2,30 @@
    
    import { RiAdminFill } from "react-icons/ri";
 
-
+import "./Login.css"
 export const Login=()=>{
     return(
         <>
-       
+
+        <section className="login-box">
+        <div className="icon">
+        <RiAdminFill />
+        </div>
+          <form method="Post">
+        <label htmlFor="username"></label>
+        <input type="text" name="username" placeholder="Enter the username" />
+        <label htmlFor="password "></label>
+        <input type="password" name="password" placeholder="Enter the Password"/>
+        <button>Submit</button>
+      </form>
+      </section>
+
+
+
+
+
+
+
         
         
         
